a293b2cd26c9d1cbb1a6e61095ae94558c13bc1b
Razvan Pocaznoi <rpocaznoi@gmail.com>
2018-01-19 17:21:03 +0200

    added entities as props to the db object merge the ones from options with the ones found in the data folder

--- src/db.js
+++ src/db.js
@@ -1,26 +1,35 @@
 const fs = require('fs')
-, createEntity = require('./entity')
+  , createEntity = require('./entity')
+  , _ = require('lodash')
+
 class fsdb {
 
   constructor(options) {
-    const { datadir } = options
+    const { datadir, entities } = options
     this.datadir = datadir
-    if(!fs.existsSync(datadir))
+    if (!fs.existsSync(datadir))
       fs.mkdirSync(datadir)
-    this.entities = fs.readdirSync(datadir)
+    const entitiesInDataDir = fs.readdirSync(datadir).map(f => f.replace(/(.*)\.json/, '$1'))
+      , all = _.union(entities, entitiesInDataDir).filter(e => e !== 'ensure')
+
+    if (all.length) {
+      all.forEach(entity => {
+        this[entity] = createEntity(`${this.datadir}/${entity}`)
+      })
+    }
   }
 
   ensure(entity) {
-    if(!this.entities[entity]){
-      this.entities[entity] = createEntity(`${this.datadir}/${entity}`)
+    if (!this[entity]) {
+      this[entity] = createEntity(`${this.datadir}/${entity}`)
     }
-    return this.entities[entity]
+    return this[entity]
   }
 
 }
 
 module.exports = (options) => {
-  const { datadir: optionsDataDir } = options
+  const { datadir: optionsDataDir, entities } = options
     , datadir = optionsDataDir || 'yafsdbdata'
-  return new fsdb({datadir})
+  return new fsdb({ datadir, entities })
 }
