4b2651dba1f31fb9aae3017523e83b6cff890d93
Razvan Pocaznoi <rpocaznoi@gmail.com>
2018-01-27 21:11:03 +0200

    fixed wrong import

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const { fsdb, createDb } = require('./src/db')
-const { entity, createEntity } = require('./src/db')
+const { entity, createEntity } = require('./src/entity')
 module.exports = { fsdb, entity, createEntity, createDb }
